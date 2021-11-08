23a81136c90e2d56a2b67157939d7ca3275d7407
Michael Jeronimo <michael.jeronimo@openrobotics.org>
2021-11-08 11:29:03 -0800

    Some code reformatting
    Signed-off-by: Michael Jeronimo <michael.jeronimo@openrobotics.org>

--- packages/studio-base/src/panels/SystemView/index.tsx
+++ packages/studio-base/src/panels/SystemView/index.tsx
@@ -4,7 +4,6 @@ import { MouseEvent, useState, useCallback, CSSProperties, useRef } from "react"
 // FluentUI
 import { Stack } from "@fluentui/react";
 import { DefaultButton } from '@fluentui/react/lib/Button';
-import { useBoolean } from '@fluentui/react-hooks';
 
 // Foxglove
 import Panel from "@foxglove/studio-base/components/Panel";
@@ -16,26 +15,16 @@ import { Canvas, CanvasDirection, CanvasRef, Node, Edge, MarkerArrow, Port, Icon
 
 // SystemView
 import helpContent from "./index.help.md";
-import RosNode from "./RosNode";
-import RosTopic from "./RosTopic";
 import NodePanel from "./NodePanel";
 import { ws_connect, ws_disconnect } from "./WebSocketClient"
-
-//const onLoad = (reactFlowInstance: OnLoadParams) => {
-//  ws_connect();
-//  console.log('flow loaded:', reactFlowInstance);
-//}
-
 
 const canvasRef = React.createRef<CanvasRef>();
 const onLayout = (direction: CanvasDirection) => {
   const canvas = canvasRef.current!;
   console.log(canvas);
-
   if (canvas.fitCanvas) {
     canvas.fitCanvas();
   }
-
   console.log(canvas.layout);
 }
 
@@ -56,8 +45,7 @@ type Props = {
   saveConfig: (arg0: Config) => void;
 };
 
-const SystemViewPanel = React.memo(({ config, saveConfig }: Props) =>
-{
+const SystemViewPanel = React.memo(({ config, saveConfig }: Props) => {
   // TODO: configuration
   const { minLogLevel, searchTerms } = config;
 
@@ -69,131 +57,134 @@ const SystemViewPanel = React.memo(({ config, saveConfig }: Props) =>
       <DefaultButton text="Fit" style={fitStyle} />
       <DefaultButton text="Center" style={centerStyle} />
       <Stack grow>
-      <div
-      style={{
-          position: 'absolute',
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          // backgroundColor: 'blue',
-          // backgroundImage: 'repeating-radial-gradient(top center,rgba(1.0,0,0,.2),rgba(1.0,0,0,.2) 10px, transparent 0, transparent 100%)',
-          // background: 'repeating-radial-gradient(red, yellow 10%, green 15%)'
-
-          // background: '666',
-          // backgroundImage: 'linear-gradient(45deg, #bbb 25%, transparent 0), linear-gradient(45deg, transparent 75%, #bbb 0)',
-          // backgroundPosition: '0 0, 25px 25px',
-          // backgroundSize: '30px 30px',
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
           }} >
-    <Canvas
-      // required to enable edges from/to nested nodes
-      ref={canvasRef}
-      pannable={true}
-      fit={true}
-      center={true}
-      nodes={[
-          { id: '3', text: 'stereo_camera_controller',
-            icon: {
-                url: 'https://s3.amazonaws.com/img.crft.app/package-slack-logo-bw.svg',
-                height: 25,
-                width: 25
+          <Canvas
+            // required to enable edges from/to nested nodes
+            ref={canvasRef}
+            pannable={true}
+            fit={true}
+            center={true}
+            nodes={[
+              {
+                id: '3', text: 'stereo_camera_controller',
+                icon: {
+                  url: 'https://s3.amazonaws.com/img.crft.app/package-slack-logo-bw.svg',
+                  height: 25,
+                  width: 25
                 },
-          },
-          { id: '2', text: '/left/image_raw',
-            icon: {
-                url: 'https://s3.amazonaws.com/img.crft.app/package-flashpoint-logo-bw.svg',
-                height: 25,
-                width: 25
+              },
+              {
+                id: '2', text: '/left/image_raw',
+                icon: {
+                  url: 'https://s3.amazonaws.com/img.crft.app/package-flashpoint-logo-bw.svg',
+                  height: 25,
+                  width: 25
                 },
-          },
-          { id: '4', text: '/right/image_raw',
-            icon: {
-                url: 'https://s3.amazonaws.com/img.crft.app/package-flashpoint-logo-bw.svg',
-                height: 25,
-                width: 25
+              },
+              {
+                id: '4', text: '/right/image_raw',
+                icon: {
+                  url: 'https://s3.amazonaws.com/img.crft.app/package-flashpoint-logo-bw.svg',
+                  height: 25,
+                  width: 25
                 },
-          },
-          { id: '5', text: 'image_adjuster_left_stereo',
-            icon: {
-                url: 'https://s3.amazonaws.com/img.crft.app/package-slack-logo-bw.svg',
-                height: 25,
-                width: 25
+              },
+              {
+                id: '5', text: 'image_adjuster_left_stereo',
+                icon: {
+                  url: 'https://s3.amazonaws.com/img.crft.app/package-slack-logo-bw.svg',
+                  height: 25,
+                  width: 25
                 }
-          },
-          { id: '7', text: 'image_adjuster_right_stereo',
-            icon: {
-                url: 'https://s3.amazonaws.com/img.crft.app/package-slack-logo-bw.svg',
-                height: 25,
-                width: 25
+              },
+              {
+                id: '7', text: 'image_adjuster_right_stereo',
+                icon: {
+                  url: 'https://s3.amazonaws.com/img.crft.app/package-slack-logo-bw.svg',
+                  height: 25,
+                  width: 25
                 }
-          },
-          { id: '8', text: 'disparity_node',
-            icon: {
-                url: 'https://s3.amazonaws.com/img.crft.app/package-slack-logo-bw.svg',
-                height: 25,
-                width: 25
+              },
+              {
+                id: '8', text: 'disparity_node',
+                icon: {
+                  url: 'https://s3.amazonaws.com/img.crft.app/package-slack-logo-bw.svg',
+                  height: 25,
+                  width: 25
                 }
-          },
-          { id: '9', text: 'point_cloud_node',
-            icon: {
-                url: 'https://s3.amazonaws.com/img.crft.app/package-slack-logo-bw.svg',
-                height: 25,
-                width: 25
+              },
+              {
+                id: '9', text: 'point_cloud_node',
+                icon: {
+                  url: 'https://s3.amazonaws.com/img.crft.app/package-slack-logo-bw.svg',
+                  height: 25,
+                  width: 25
                 }
-          },
-          { id: '10', text: '/left/image_raw/adjusted_stereo',
-            icon: {
-                url: 'https://s3.amazonaws.com/img.crft.app/package-flashpoint-logo-bw.svg',
-                height: 25,
-                width: 25
+              },
+              {
+                id: '10', text: '/left/image_raw/adjusted_stereo',
+                icon: {
+                  url: 'https://s3.amazonaws.com/img.crft.app/package-flashpoint-logo-bw.svg',
+                  height: 25,
+                  width: 25
                 }
-          },
-          { id: '11', text: '/right/image_raw/adjusted_stereo',
-            icon: {
-                url: 'https://s3.amazonaws.com/img.crft.app/package-flashpoint-logo-bw.svg',
-                height: 25,
-                width: 25
+              },
+              {
+                id: '11', text: '/right/image_raw/adjusted_stereo',
+                icon: {
+                  url: 'https://s3.amazonaws.com/img.crft.app/package-flashpoint-logo-bw.svg',
+                  height: 25,
+                  width: 25
                 }
-          },
-          { id: '12', text: '/disparity',
-            icon: {
-                url: 'https://s3.amazonaws.com/img.crft.app/package-slack-logo-bw.svg',
-                height: 25,
-                width: 25
+              },
+              {
+                id: '12', text: '/disparity',
+                icon: {
+                  url: 'https://s3.amazonaws.com/img.crft.app/package-slack-logo-bw.svg',
+                  height: 25,
+                  width: 25
                 }
-          },
-          { id: '13', text: '/points2',
-            icon: {
-                url: 'https://s3.amazonaws.com/img.crft.app/package-slack-logo-bw.svg',
-                height: 25,
-                width: 25
+              },
+              {
+                id: '13', text: '/points2',
+                icon: {
+                  url: 'https://s3.amazonaws.com/img.crft.app/package-slack-logo-bw.svg',
+                  height: 25,
+                  width: 25
                 }
-          },
+              },
 
-      ]}
-      edges={[
-          { id: 'e3-4', from: '3', to: '4', text: '10 Hz' },
-          { id: 'e3-2', from: '3', to: '2', text: '11 Hz' },
-          { id: 'e4-7', from: '4', to: '7', text: '12 Hz' },
-          { id: 'e2-5', from: '2', to: '5', text: '13 Hz' },
-          { id: 'e7-11', from: '7', to: '11', text: '14 Hz' },
-          { id: 'e10-8', from: '10', to: '8', text: '15 Hz' },
-          { id: 'e10-9', from: '10', to: '9', text: '16 Hz' },
-          { id: 'e5-10', from: '5', to: '10', text: '17 Hz' },
-          { id: 'e11-8', from: '11', to: '8', text: '18 Hz' },
-          { id: 'e12-9', from: '12', to: '9', text: '19 Hz' },
-          { id: 'e8-12', from: '8', to: '12', text: '20 Hz' },
-          { id: 'e9-13', from: '9', to: '13', text: '21 Hz' },
-      ]}
-      node={
-        <Node
-          icon={<Icon />}
-        />
-      }
+            ]}
+            edges={[
+              { id: 'e3-4', from: '3', to: '4', text: '10 Hz' },
+              { id: 'e3-2', from: '4', to: '2', text: '11 Hz' },
+              { id: 'e4-7', from: '4', to: '7', text: '12 Hz' },
+              { id: 'e2-5', from: '2', to: '5', text: '13 Hz' },
+              { id: 'e7-11', from: '7', to: '11', text: '14 Hz' },
+              { id: 'e10-8', from: '10', to: '8', text: '15 Hz' },
+              { id: 'e10-9', from: '10', to: '9', text: '16 Hz' },
+              { id: 'e5-10', from: '5', to: '10', text: '17 Hz' },
+              { id: 'e11-8', from: '11', to: '8', text: '18 Hz' },
+              { id: 'e12-9', from: '12', to: '9', text: '19 Hz' },
+              { id: 'e8-12', from: '8', to: '12', text: '20 Hz' },
+              { id: 'e9-13', from: '9', to: '13', text: '21 Hz' },
+            ]}
+            node={
+              <Node
+                icon={<Icon />}
+              />
+            }
 
-      direction={"DOWN"}
-      onLayoutChange={layout => console.log('Layout', layout)} />
-      </div>
+            direction={"DOWN"}
+            onLayoutChange={layout => console.log('Layout', layout)} />
+        </div>
         <NodePanel />
       </Stack>
     </Stack>
